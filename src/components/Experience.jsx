6b65a5047145de0d5a622e09333b5e34d3460f49
Bhargavi-Chaudhary-803 <bhargavichaudhary803@gmail.com>
2026-07-04 19:35:35 +0000

    Jul 5, 2026, 1:05 AM

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Experience() {
   return (
-    <section className="min-h-screen px-6 pt-16 bg-transparent">
+    <section className="min-h-screen px-6 pt-[-12] bg-transparent">
       <div className="max-w-7xl mx-auto mt-[50px]">
 
         {/* 1. Left aligned: "Hi! I am" */}
