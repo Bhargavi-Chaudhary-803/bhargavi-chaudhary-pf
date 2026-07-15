8c1b94e948a77a87d59ab88e80b91ddadf42abf0
Bhargavi-Chaudhary-803 <bhargavichaudhary803@gmail.com>
2026-07-15 17:56:14 +0000

    Jul 15, 2026, 11:26 PM

--- src/app/api/visitor/route.js
+++ src/app/api/visitor/route.js
@@ -1,5 +1,4 @@
 import { Redis } from "@upstash/redis";
-import { cookies } from "next/headers";
 
 const redis = new Redis({
   url: process.env.KV_REST_API_URL,
@@ -7,25 +6,7 @@ const redis = new Redis({
 });
 
 export async function GET() {
-  const cookieStore = await cookies();
-
-  const alreadyVisited = cookieStore.get("portfolio-visited");
-
-  let count = await redis.get("visitor-count");
-
-  if (!count) {
-    count = 0;
-  }
-
-  if (!alreadyVisited) {
-    count = await redis.incr("visitor-count");
-
-    cookieStore.set("portfolio-visited", "true", {
-      maxAge: 60 * 60 * 24 * 30, // 30 days
-      httpOnly: true,
-      sameSite: "lax",
-    });
-  }
+  const count = await redis.incr("visitor-count");
 
   return Response.json({
     visitors: Number(count),
